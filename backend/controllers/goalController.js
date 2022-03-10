7fff4e9e7b38200c2887544d1d8d0cadb533488b
Mark-DSouza <markdsouza434@gmail.com>
2022-03-10 22:57:51 +0530

    add documentation (comments) to controllers in goalController.js

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,21 +1,33 @@
+// @desc    Get goals
+// @route   /api/v1/goals
+// @access  Private
 const getGoals = (req, res) => {
   res.status(200).json({
     message: "Get goals"
   });
 };
 
+// @desc    Set goals
+// @route   /api/v1/goals
+// @access  Private
 const setGoal = (req, res) => {
   res.status(200).json({
     message: "Set goal"
   });
 };
 
+// @desc    Update goals
+// @route   /api/v1/goals/:id
+// @access  Private
 const updateGoal = (req, res) => {
   res.status(200).json({
     message: `Update goal ${req.params.id}`
   });
 };
 
+// @desc    Delete goals
+// @route   /api/v1/goals/:id
+// @access  Private
 const deleteGoal = (req, res) => {
   res.status(200).json({
     message: `Delete goal ${req.params.id}`
