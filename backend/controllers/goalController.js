c2627da317dbffe961bfb12c47a82862563667c4
Mark-DSouza <markdsouza434@gmail.com>
2022-03-11 09:55:59 +0530

    make functions (actions) in goalController.js asynchronous

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,42 +1,44 @@
+const asyncHandler = require("express-async-handler");
+
 // @desc    Get goals
 // @route   /api/v1/goals
 // @access  Private
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
   res.status(200).json({
     message: "Get goals"
   });
-};
+})
 
 // @desc    Set goals
 // @route   /api/v1/goals
 // @access  Private
-const setGoal = (req, res) => {
+const setGoal = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400);
     throw new Error('Please add a text field');
   }
   res.status(200).json({
     message: "Set goal"
-  });
-};
+  })
+});
 
 // @desc    Update goals
 // @route   /api/v1/goals/:id
 // @access  Private
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
   res.status(200).json({
     message: `Update goal ${req.params.id}`
   });
-};
+})
 
 // @desc    Delete goals
 // @route   /api/v1/goals/:id
 // @access  Private
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
   res.status(200).json({
     message: `Delete goal ${req.params.id}`
   });
-};
+})
 
 module.exports = {
   getGoals,
