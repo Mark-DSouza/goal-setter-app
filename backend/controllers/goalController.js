07f21fbd1472a6d905b01f28f94865d73c682aa9
Mark-DSouza <markdsouza434@gmail.com>
2022-03-11 11:25:59 +0530

    add connection to database

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,48 +1,49 @@
-const asyncHandler = require("express-async-handler");
+const asyncHandler = require('express-async-handler');
 
 // @desc    Get goals
 // @route   /api/v1/goals
 // @access  Private
 const getGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "Get goals"
-  });
-})
+	res.status(200).json({
+		message: 'Get goals',
+	});
+});
 
 // @desc    Set goals
 // @route   /api/v1/goals
 // @access  Private
 const setGoal = asyncHandler(async (req, res) => {
-  if (!req.body.text) {
-    res.status(400);
-    throw new Error('Please add a text field');
-  }
-  res.status(200).json({
-    message: "Set goal"
-  })
+	if (!req.body.text) {
+		res.status(400);
+		throw new Error('Please add a text field');
+	}
+	res.status(200).json({
+		message: 'Set goal',
+	});
 });
 
 // @desc    Update goals
 // @route   /api/v1/goals/:id
 // @access  Private
 const updateGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: `Update goal ${req.params.id}`
-  });
-})
+	console.log('This is using the formatter.');
+	res.status(200).json({
+		message: `Update goal ${req.params.id}`,
+	});
+});
 
 // @desc    Delete goals
 // @route   /api/v1/goals/:id
 // @access  Private
 const deleteGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: `Delete goal ${req.params.id}`
-  });
-})
+	res.status(200).json({
+		message: `Delete goal ${req.params.id}`,
+	});
+});
 
 module.exports = {
-  getGoals,
-  setGoal,
-  updateGoal,
-  deleteGoal
-}
+	getGoals,
+	setGoal,
+	updateGoal,
+	deleteGoal,
+};
