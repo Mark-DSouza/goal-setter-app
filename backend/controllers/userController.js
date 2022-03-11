f738db67bc80a6e6ae18e650eb92159a38d02064
Mark-DSouza <markdsouza434@gmail.com>
2022-03-11 16:55:24 +0530

    add documentation to functions (actions) in userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,15 +1,24 @@
+// @desc    Register User
+// @route   /api/v1/users
+// @access  Public
 const registerUser = (req, res) => {
 	res.status(200).json({
 		message: 'Register User',
 	});
 };
 
+// @desc    Login User
+// @route   /api/v1/users/login
+// @access  Public
 const loginUser = (req, res) => {
 	res.status(200).json({
 		message: 'Login User',
 	});
 };
 
+// @desc    Get user data
+// @route   /api/v1/users/me
+// @access  Public
 const getMe = (req, res) => {
 	res.status(200).json({
 		message: 'Get user data',
