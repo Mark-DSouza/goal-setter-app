3b1302dbe2232a425934b3f4e12042f03321bdfc
Mark-DSouza <markdsouza434@gmail.com>
2022-03-11 16:40:42 +0530

    add routes and barebones controller for User model

---
+++ backend/controllers/userController.js
@@ -0,0 +1,23 @@
+const registerUser = (req, res) => {
+	res.status(200).json({
+		message: 'Register User',
+	});
+};
+
+const loginUser = (req, res) => {
+	res.status(200).json({
+		message: 'Login User',
+	});
+};
+
+const getMe = (req, res) => {
+	res.status(200).json({
+		message: 'Get user data',
+	});
+};
+
+module.exports = {
+	registerUser,
+	loginUser,
+	getMe,
+};
--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/v1/goals', require('./routes/goalRoutes'));
+app.use('/api/v1/users', require('./routes/userRoutes'));
 
 app.use(errorHandler);
 
